8f9f8120563b0783e2b74ee821363bfaf163bc96
Yao Chi <yaochi@hnrfqj.com>
2020-09-11 14:54:58 +0800

    更新

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
+    <div>自动化构建travis CI</div>
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
